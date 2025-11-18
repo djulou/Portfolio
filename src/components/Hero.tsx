bf735944ff675394495f2407d24504765599bcd5
djulou <dorian.julou@gmail.com>
2025-11-18 15:22:05 +0100

    Add public

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,14 +3,14 @@ import '../styles/Hero.css'
 export default function Hero() {
   return (
     <section className="hero">
-        <img className="paysage" src="/img/paysage.jpg" alt="paysage" />
+        <img className="paysage" src="/public/img/paysage.jpg" alt="paysage" />
         <div className="hero-content">
           <div className="first_texte">
             <h1>Bonjour</h1>
             <h2>Je m'appelle <span>Dorian</span></h2>
             <p>Bienvenue sur mon portfolio !</p>
           </div>
-          <img className="profil" src="/img/64.png" alt="profil" />
+          <img className="profil" src="/public/img/64.png" alt="profil" />
         </div>
         <div className="scroll-arrow">
           <span>▼</span>
