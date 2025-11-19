06687545697b45eb9e54d8e00ddd1b00da125fbb
djulou <dorian.julou@gmail.com>
2025-11-19 10:44:52 +0100

    Resolve problem path

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,19 @@
 import '../styles/Hero.css'
 import paysage from '../../public/img/paysage.jpg';
+import cv from '../../public/img/64.png';
+
 
 export default function Hero() {
   return (
     <section className="hero">
-        <img className="paysage" src="/img/paysage.jpg" alt="paysage" />
+        <img className="paysage" src={paysage} alt="paysage" />
         <div className="hero-content">
           <div className="first_texte">
             <h1>Bonjour</h1>
             <h2>Je m'appelle <span>Dorian</span></h2>
             <p>Bienvenue sur mon portfolio !</p>
           </div>
-          <img className="profil" src={paysage} alt="profil" />
+          <img className="profil" src={cv} alt="profil" />
         </div>
         <div className="scroll-arrow">
           <span>▼</span>
